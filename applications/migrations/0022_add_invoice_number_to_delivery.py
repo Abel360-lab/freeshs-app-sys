e51886d06c045496d325daf80c06b1f0d5b23189
Abel Buertey Okudjeto <Abel@hq.gcx.com.gh>
2025-10-23 18:06:35 +0000

    Remove duplicate migrations 0020 and 0021, fix migration 0022 dependency

--- applications/migrations/0022_add_invoice_number_to_delivery.py
+++ applications/migrations/0022_add_invoice_number_to_delivery.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('applications', '0021_create_waybill_table'),
+        ('applications', '0019_merge_20250925_0041'),
     ]
 
     operations = [
